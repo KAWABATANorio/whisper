b056055a046e5238a3e0e0faf9777068bd38b749
= <=>
2023-05-31 08:01:29 +0900

    measure

--- tests/ffmpeg.test.ts
+++ tests/ffmpeg.test.ts
@@ -4,18 +4,27 @@ import ffmpeg from 'fluent-ffmpeg';
 test('whisper test1', async () => {
   const input = createReadStream(__dirname + '/../recordings/imposter.ogg');
   const output = createWriteStream(__dirname + '/../recordings/imposter.webm');
+  const start = new Date();
 
-  ffmpeg()
-    .input(input)
-    .inputFormat('ogg')
-    .audioCodec('libopus')
-    .audioChannels(1)
-    .outputFormat('webm')
-    .on('error', (err: Error) => {
-      console.warn(`❌ Error recording file - ${err.message}`);
-    })
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    .on('end', () => {})
-    .pipe(output, { end: true });
+  await (async () => {
+    return new Promise((resolve, reject) => {
+      ffmpeg()
+        .input(input)
+        .inputFormat('ogg')
+        .audioCodec('libopus')
+        .audioChannels(1)
+        .outputFormat('webm')
+        .on('error', (err: Error) => {
+          console.warn(`❌ Error recording file - ${err.message}`);
+          reject(err);
+        })
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        .on('end', () => {
+          resolve(0);
+          console.log(`✅ ${(new Date().getTime() - start.getTime()).toLocaleString()} ms`);
+        })
+        .pipe(output, { end: true });
+    });
+  })();
 
 }, 10000);
