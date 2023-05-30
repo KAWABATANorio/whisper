8720996d975d0c5e32663b12d9213bdde9763830
= <=>
2023-05-31 08:15:11 +0900

    tests

--- tests/ffmpeg.test.ts
+++ tests/ffmpeg.test.ts
@@ -1,12 +1,12 @@
 import { createReadStream, createWriteStream } from 'node:fs';
 import ffmpeg from 'fluent-ffmpeg';
 
-test('whisper test1', async () => {
+async function ogg2webm(): Promise<number> {
   const input = createReadStream(__dirname + '/../recordings/imposter.ogg');
   const output = createWriteStream(__dirname + '/../recordings/imposter.webm');
   const start = new Date();
 
-  await (async () => {
+  return await (async () => {
     return new Promise((resolve, reject) => {
       ffmpeg()
         .input(input)
@@ -20,11 +20,26 @@ test('whisper test1', async () => {
         })
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         .on('end', () => {
-          resolve(0);
-          console.log(`✅ ${(new Date().getTime() - start.getTime()).toLocaleString()} ms`);
+          const measure = new Date().getTime() - start.getTime();
+          resolve(measure);
         })
         .pipe(output, { end: true });
     });
   })();
+}
 
+test('ffmpeg', async () => {
+  const measure = await ogg2webm();
+  console.log(`✅ ${measure.toLocaleString()} ms`);
+});
+
+test('measure', async () => {
+  let total = 0;
+  const times = 10;
+  for (let n = 0; n < times; n++) {
+    const measure = await ogg2webm();
+    total += measure;
+    console.log(`✅ ${measure.toLocaleString()} ms`);
+  }
+  console.log(`Average: ${(total / times).toLocaleString()} ms`);
 }, 10000);
--- tests/whisper.test.ts
+++ tests/whisper.test.ts
@@ -1,6 +1,9 @@
 import { whisper } from '../src/whisper'
 
-test('whisper test', async () => {
+test('whisper', async () => {
+  const start = new Date();
   const text = await whisper(__dirname + '/../recordings/imposter.webm');
+  const measure = new Date().getTime() - start.getTime();
   console.log(text);
+  console.log(`✅ ${measure.toLocaleString()} ms`);
 }, 10000);
