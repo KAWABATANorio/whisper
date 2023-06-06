dbe667b363d0db523bbec9e5eb6a15843cad7c53
= <=>
2023-06-07 08:11:22 +0900

    fix ffmpeg issue

--- src/ffmpeg/index.ts
+++ src/ffmpeg/index.ts
@@ -4,27 +4,23 @@ import ffmpeg from 'fluent-ffmpeg';
 export function opusStreamToWebm(inputStream: ReadStream, filename: string): Promise<void> {
   console.log(`👂 Started recording ${filename}`);
   return new Promise<void>((resolve, reject) => {
-    try {
-      const out = createWriteStream(filename);
-      const start = new Date();
-  
-      ffmpeg()
-        .input(inputStream)
-        .inputFormat('ogg')
-        .audioCodec('libopus')
-        .audioChannels(2)
-        .outputFormat('webm')
-        .on('error', (e: Error) => {
-          throw e;
-        })
-        .on('end', () => {
-          console.log(`✅ Recorded ${filename}, ${(new Date().getTime() - start.getTime()).toLocaleString()} ms`);
-          resolve();
-        })
-        .pipe(out, { end: true });
-    } catch (e) {
-      console.warn(`❌ Error recording file ${filename} - ${(e as Error).message}`);
-      reject(e);
-    }
+    const out = createWriteStream(filename);
+    const start = new Date();
+
+    ffmpeg()
+      .input(inputStream)
+      .inputFormat('ogg')
+      .audioCodec('libopus')
+      .audioChannels(2)
+      .outputFormat('webm')
+      .on('error', (e: Error) => {
+        console.warn(`❌ Error recording file ${filename} - ${(e as Error).message}`);
+        reject(e);
+      })
+      .on('end', () => {
+        console.log(`✅ Recorded ${filename}, ${(new Date().getTime() - start.getTime()).toLocaleString()} ms`);
+        resolve();
+      })
+      .pipe(out, { end: true });
   });
 }
